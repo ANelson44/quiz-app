457270d5d260d87518e43a8c56662675af632919
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2023-10-26 12:13:14 -0400

    added timer, end game html and js

--- assets/game.js
+++ assets/game.js
@@ -1,11 +1,17 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const timerText = document.getElementById('timer');
+const progressBarFull = document.getElementById('progressBarFull');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+let timer;
+let timerCount;
 
 let questions = [
     {
@@ -53,20 +59,29 @@ let questions = [
 // Constants
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
+const TIME_PENALTY = -10;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
+    timerCount = 60
     availableQuestions = [...questions];
     getNewQuestion();
+    startTimer();
 };
 
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score);
         // go to the end page
         return window.location.assign('/end.html');
     }
     questionCounter++;
+    progressText.innerText = questionCounter + '/' + MAX_QUESTIONS;
+    // Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -81,6 +96,15 @@ getNewQuestion = () => {
     acceptingAnswers = true;
 };
 
+// Timer
+
+function startTimer() {
+    timer = setInterval(function() {
+        timerCount--;
+        timerText.textContent = timerCount;
+    },1000);
+}
+
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
         if (!acceptingAnswers) return;
@@ -92,6 +116,12 @@ choices.forEach(choice => {
         const classToApply = 
             selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+            if(classToApply === 'correct') {
+                incrementScore(CORRECT_BONUS);
+            } else {
+                decrementTime(TIME_PENALTY);
+            }
+
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
@@ -100,5 +130,16 @@ choices.forEach(choice => {
         }, 1000);
     })
 });
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
+
+decrementTime = TIME_PENALTY => {
+    timerCount -= 10
+}
+
+// ADD TIMER STOP AT 0 => LOSE
 
 startGame ();
