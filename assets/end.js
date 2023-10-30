51757783d8b40b015a45f257cf2e0446c45eb7ae
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2023-10-30 10:36:59 -0400

    added high score html to view the top 5 scores

--- assets/end.js
+++ assets/end.js
@@ -4,6 +4,7 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+console.log(highScores);
 const MAX_HIGH_SCORES =5;
 finalScore.innerText = mostRecentScore;
 
@@ -15,13 +16,15 @@ saveHighScore = e => {
     e.preventDefault();
 
     const score = {
-        score: Math.floor(Math.random() * 100),
+        score: mostRecentScore,
         name: username.value
     };
     highScores.push(score);
     highScores.sort ( (a,b) => b.score - a.score)
     highScores.splice(5);
 
-    localStorage.setItem('highscores', JSON.stringify(highScores));
+    console.log(highScores);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.assign('./index.html');
 };
